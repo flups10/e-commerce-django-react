98cb48a7dda754c6477ce7db1a49eba9023a659e
flups10 <je.tieleman@gmail.com>
2024-02-29 13:48:44 +0100

    added redux to homescreen

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .products import products
+from .models import Product
+
+from .serializer import ProdcutSerializer
 
 # Create your views here.
 
@@ -27,14 +29,13 @@ def get_routes(request):
 
 @api_view(['GET'])
 def get_products(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProdcutSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def get_product(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProdcutSerializer(product, many=False)
+    return Response(serializer.data)
