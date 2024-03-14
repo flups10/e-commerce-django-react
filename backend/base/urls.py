ebb1fbe8a83b563dd3870c2cfdf6c6a54b1f9ea4
flups10 <je.tieleman@gmail.com>
2024-03-15 00:05:15 +0100

    finished cart

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('', views.get_routes, name='routes'),
     path('products/', views.get_products, name='products'),
     path('product/', views.get_product, name='product'),
-
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
